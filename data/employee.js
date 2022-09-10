11e9c50e9932581174124c3fe8fd0dd0a189571b
Nathaly Reyes <andrea.reyes94@hotmail.com>
2022-09-10 19:44:02 -0400

    Added employee data

---
+++ data/employee.js
@@ -0,0 +1,62 @@
+let employeeData = [
+    {
+        name: 'Christine Vera',
+        pic: '',
+        dateOfJoining: 'Oct-10-2020',
+        position: 'Software Engineer (I)',
+        team: 'A',
+        hobbies: ["Travelling","Dog mon","Yoga","self-taught coder"],
+        areasOfExpertise: ["JavaScript","C++","HTML","CSS"],
+        languagesSpoken: ["English","French"]
+    },
+    {
+        name: 'Vienna Sharon',
+        pic: '',
+        dateOfJoining: 'Jan-3-2015',
+        position: 'Senior Software Engineer',
+        team: 'A',
+        hobbies: ["Hiking","Dog person","Baking"],
+        areasOfExpertise: ["Database Design","Java","DevOps"],
+        languagesSpoken: ["English","Italian"]
+    },
+    {
+        name: 'MaryAnn Lizzi',
+        pic: '',
+        dateOfJoining: 'Aug-15-2016',
+        position: 'Senior Software Engineer',
+        team: 'B',
+        hobbies: ["Salsa","Painting","Yoga"],
+        areasOfExpertise: ["UX/UI","JavaScript","React"],
+        languagesSpoken: ["English","Portuguese","French"]
+    },
+    {
+        name: 'Andrea Rosen',
+        pic: '',
+        dateOfJoining: 'Feb-05-2015',
+        position: 'Senior Software Engineer',
+        team: 'B',
+        hobbies: ["Hiking","Dog person","Baking"],
+        areasOfExpertise: ["Algorithms","Data Structures"],
+        languagesSpoken: ["English","Spanish"]
+    },
+    {
+        name: 'Joelle Margaret',
+        pic: '',
+        dateOfJoining: 'May-20-2017',
+        position: 'Senior Software Engineer',
+        team: 'C',
+        hobbies: ["Salsa","Painting","Yoga"],
+        areasOfExpertise: ["Nodejs","C#","PostgreSQL"],
+        languagesSpoken: ["English"]
+    },
+    {
+        name: 'Josie Rowen',
+        pic: '',
+        dateOfJoining: 'Mar-8-2018',
+        position: 'Head Software Engineer',
+        team: 'C',
+        hobbies: ["Language Learning","Yoga"],
+        areasOfExpertise: ["Software Design","Testing","C"],
+        languagesSpoken: ["English","French"],
+    }
+]
