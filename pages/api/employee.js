28320512613dc2dd037ceab4ff291ec092f2e905
Manvinderjit Singh Rupal <manvinderjit@gmail.com>
2022-09-10 19:44:55 -0400

    Calendar Branch Creation Commit

--- pages/api/employee.js
+++ pages/api/employee.js
@@ -1,18 +1,34 @@
 let employeeData = [
     {
         name: 'Jino John',
+        pic: '',
         doj: 'Jan-3rd-2022',
         position: 'Junior Developer',
-        team: 'A',        
+        team: 'A',
+        skills: ['MySQL', 'NodeJS', 'PHP'],
+        // skillExperienceMonths: [3, 4, 5],
+        areasOfInterest: [],
+        hobbies: [],        
     },
     {
         name: 'Nathaly Reyes',
         doj: 'April-1st-2022',
         position: 'Front-End Developer',
-        team: 'A'
+        team: 'A',
+        skills: ['MongoDB', 'NodeJS', 'Express'],
+        areasOfInterest: [],        
+    },
+    {
+        name: 'Agnes',
+        doj: 'April-1st-2022',
+        position: 'UI/UX Designer',
+        team: 'B',
+        skills: ['MongoDB', 'NodeJS', 'Express'],
     }
 ]
 
+
+export { employeeData };
 
 export default function handler(req, res) {
     res.status(200).json(employeeData);
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import { employeeData } from "./employee";
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(employeeData)
 }
